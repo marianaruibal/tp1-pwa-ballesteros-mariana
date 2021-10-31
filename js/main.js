e334e2967a8fa0a4235f83d780dae7cb3a220964
marianaruibal <89877848+marianaruibal@users.noreply.github.com>
2021-10-31 15:15:53 -0300

    Optimización del código

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@ const URL_MAPS = "https://www.google.com/maps/embed/v1/";
 
 const button = document.getElementById('enviar')
 const inputValor = document.getElementById('busqueda');
+const h2 = document.querySelector('#weather h2');
+const pDescrip = document.querySelector('#temp div:last-of-type p:last-of-type');
+const img = document.querySelector('#temp div img');
+const tempe = document.querySelector('#temp div:last-of-type p:first-of-type');
+const divVideo = document.querySelector('#weather');
+const allspan = document.querySelectorAll('#data ul li > span:last-of-type');
 
 const ultimaCiudadBuscada = JSON.parse(localStorage.getItem("ciudad"));
 
     console.log(ultimaCiudadBuscada);
 
-if(ultimaCiudadBuscada == null){
-    byDefault();
-}else{
+
     buscarCiudad(ultimaCiudadBuscada);
-}
 
 
 button.addEventListener('click', ()=>{
@@ -23,10 +26,10 @@ button.addEventListener('click', ()=>{
     buscarCiudad(inputValor.value);
 })
 
-function byDefault(){
-    buscarCiudad('Buenos Aires');
-}
-
+/**
+ * Recibe la ciudad ingresada por el usuario y trae los datos de la API
+ * @param ciudad
+ */
 function buscarCiudad(ciudad){
 
         //console.log('ciudad:', ciudad);
@@ -50,265 +53,25 @@ function buscarCiudad(ciudad){
      });
 }
 
+/**
+ * Setea los datos devueltos por la API dentro del HTML
+ * @param datos
+ */
 function clima(datos){
-    const h2 = document.querySelector('#weather h2');
-    const pDescrip = document.querySelector('#temp div:last-of-type p:last-of-type');
-    const img = document.querySelector('#temp div img');
-    const tempe = document.querySelector('#temp div:last-of-type p:first-of-type');
-    const divVideo = document.querySelector('#weather');
-    const allspan = document.querySelectorAll('#data ul li > span:last-of-type');
-    const allicons = document.querySelectorAll('#data ul li > span:first-of-type');
+
     console.log(datos);
 
     if(document.querySelector('#weather video')){
         divVideo.removeChild(document.querySelector('#weather video'));
     }
 
+    let horaActual = getHour(datos.timezone);
 
-    let horaUtc= new Date().getUTCHours();
-    console.log('horaUtc',horaUtc);
-
-    let pasarSec = datos.timezone/3600;
-
-    var horaActual = horaUtc + pasarSec;
-
-    console.log(horaActual);
-
-    if(horaActual > 24){
-        let horaResta = horaActual - 24;
-        horaActual = 0 + horaResta;
-        console.log(horaActual);
-    }else if(horaActual < 0){
-        let hora = 24 + horaActual;
-        horaActual = hora;
-        console.log(horaActual);
-    }else{
-        console.log('sin modificar:', horaActual);
-    }
 
     h2.innerHTML = `${datos.name} - ${datos.sys.country}`;
     pDescrip.innerHTML = datos.weather[0].description;
 
-    if(horaActual >= 6 && horaActual <= 19){
-        if(datos.weather[0].id === 800){
-            img.src='img/soleado.png';
-            img.alt='Pleno sol';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/sunny.mp4";
-            source.type="video/mp4";
-
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id > 800 && datos.weather[0].id < 805){
-            img.src='img/nublado.png';
-            img.alt='Sol cubierto por nubes de lluvia';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/dia-nublado.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if( datos.weather[0].id === 511 ||datos.weather[0].id >= 600 && datos.weather[0].id <= 602 || datos.weather[0].id >= 611 && datos.weather[0].id <= 613 || datos.weather[0].id === 615 || datos.weather[0].id === 616 || datos.weather[0].id >= 620 && datos.weather[0].id <= 622){
-            img.src='img/nieve.png';
-            img.alt='Nube con copos de nieve';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/nieve-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id >= 300 && datos.weather[0].id <= 302 || datos.weather[0].id >= 310 && datos.weather[0].id <= 314 || datos.weather[0].id === 321 || datos.weather[0].id >= 500 && datos.weather[0].id <= 504 || datos.weather[0].id >= 520 && datos.weather[0].id <= 522 || datos.weather[0].id === 531){
-            img.src='img/lluvia.png';
-            img.alt='Nube de lluvia';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/lluvia-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id >= 200 && datos.weather[0].id <= 202 || datos.weather[0].id >= 210 && datos.weather[0].id <= 212 || datos.weather[0].id === 221 || datos.weather[0].id >= 230 && datos.weather[0].id <= 232){
-            img.src='img/tormenta.png';
-            img.alt='Nube de tormenta';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/lluvia-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id === 781){
-            img.src='img/tornado.png';
-            img.alt='Tornado';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/lluvia-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else{
-            img.src='img/niebla.png';
-            img.alt='Sol cubierto por nubes oscuras';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/dia-niebla.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }
-    }else{
-        if(datos.weather[0].id === 800){
-            img.src='img/noche_despejada.png';
-            img.alt='Luna con estrellas despejadas';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/moon.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-
-        }else if(datos.weather[0].id > 800 && datos.weather[0].id < 805){
-            img.src='img/noche-nublado.png';
-            img.alt='Luna cubierta por nubes de lluvia';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/nublado.mp4";
-            source.type="video/mp4";
-
-            video.appendChild(source);
-
-
-        }else if( datos.weather[0].id === 511 ||datos.weather[0].id >= 600 && datos.weather[0].id <= 602 || datos.weather[0].id >= 611 && datos.weather[0].id <= 613 || datos.weather[0].id === 615 || datos.weather[0].id === 616 || datos.weather[0].id >= 620 && datos.weather[0].id <= 622){
-            img.src='img/nieve.png';
-            img.alt='Nube con copos de nieve';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/nieve-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-
-        }else if(datos.weather[0].id >= 300 && datos.weather[0].id <= 302 || datos.weather[0].id >= 310 && datos.weather[0].id <= 314 || datos.weather[0].id === 321 || datos.weather[0].id >= 500 && datos.weather[0].id <= 504 || datos.weather[0].id >= 520 && datos.weather[0].id <= 522 || datos.weather[0].id === 531){
-            img.src='img/lluvia.png';
-            img.alt='Nube de lluvia';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/nublado.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id >= 200 && datos.weather[0].id <= 202 || datos.weather[0].id >= 210 && datos.weather[0].id <= 212 || datos.weather[0].id === 221 || datos.weather[0].id >= 230 && datos.weather[0].id <= 232){
-            img.src='img/tormenta.png';
-            img.alt='Nube de tormenta';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/lluvia-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else if(datos.weather[0].id === 781){
-            img.src='img/tornado.png';
-            img.alt='Tornado';
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/lluvia-2.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }else{
-
-            img.src='img/noche-niebla.png';
-            img.alt='Luna cubierta por nubes oscuras';
-
-            let video = document.createElement('video');
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            divVideo.appendChild(video);
-
-            let source = document.createElement('source');
-            source.src="img/luna-niebla.mp4";
-            source.type="video/mp4";
-            video.appendChild(source);
-
-        }
-    }
+    getImgAndVideo(horaActual, datos.weather);
 
     tempe.innerHTML = `${datos.main.temp}°C` ;
 
@@ -324,7 +87,139 @@ function guardarLocal(datos){
     localStorage.setItem('ciudad', JSON.stringify(datos.name));
 }
 
+/**
+ * Obtiene la zona horario de la ciudad elegida
+ *
+ * @param timeZone
+ * @returns {number}
+ */
+function getHour(timeZone){
+    let horaUtc= new Date().getUTCHours();
 
+    let pasarSec = timeZone/3600;
+
+    var horaActual = horaUtc + pasarSec;
+
+
+    if(horaActual > 24){
+
+        horaActual = horaActual - 24;
+        return horaActual;
+
+    }else if(horaActual < 0){
+
+        horaActual = 24 + horaActual;
+
+        return horaActual;
+    }
+
+    return horaActual;
+}
+
+/**
+ * Genera la imagen y el video en base al id del clima devuelto por la API y el horario.
+ * @param horaActual
+ * @param datosWeater
+ */
+function getImgAndVideo(horaActual, datosWeater){
+    if(horaActual >= 6 && horaActual <= 19){
+
+        if(datosWeater[0].id === 800){
+            generateImgAndVideo('img/soleado.png', 'Pleno sol', 'img/sunny.mp4');
+
+        }else if(datosWeater[0].id > 800 && datosWeater[0].id < 805){
+            generateImgAndVideo('img/nublado.png', 'Sol cubierto por nubes de lluvia', 'img/dia-nublado.mp4');
+
+
+        }else if( datosWeater[0].id === 511 ||datosWeater[0].id >= 600 && datosWeater[0].id <= 602 || datosWeater[0].id >= 611 && datosWeater[0].id <= 613 || datosWeater[0].id === 615 || datosWeater[0].id === 616 || datosWeater[0].id >= 620 && datosWeater[0].id <= 622){
+            generateImgAndVideo('img/nieve.png', 'Nube con copos de nieve', 'img/nieve-2.mp4');
+
+
+        }else if(datosWeater[0].id >= 300 && datosWeater[0].id <= 302 || datosWeater[0].id >= 310 && datosWeater[0].id <= 314 || datosWeater[0].id === 321 || datosWeater[0].id >= 500 && datosWeater[0].id <= 504 || datosWeater[0].id >= 520 && datosWeater[0].id <= 522 || datosWeater[0].id === 531){
+
+            generateImgAndVideo('img/lluvia.png', 'Nube de lluvia', 'img/lluvia-2.mp4');
+
+        }else if(datosWeater[0].id >= 200 && datosWeater[0].id <= 202 || datosWeater[0].id >= 210 && datosWeater[0].id <= 212 || datosWeater[0].id === 221 || datosWeater[0].id >= 230 && datosWeater[0].id <= 232){
+
+            generateImgAndVideo('img/tormenta.png', 'Nube de tormenta', 'img/lluvia-2.mp4');
+
+        }else if(datosWeater[0].id === 781){
+
+            generateImgAndVideo('img/tornado.png', 'Tornado', 'img/lluvia-2.mp4');
+
+        }else{
+
+            generateImgAndVideo('img/niebla.png', 'Sol cubierto por nubes oscuras', 'img/dia-niebla.mp4');
+
+        }
+    }else{
+        if(datosWeater[0].id === 800){
+
+            generateImgAndVideo('img/noche_despejada.png', 'Luna con estrellas despejadas', 'img/moon.mp4');
+
+
+        }else if(datosWeater[0].id > 800 && datosWeater[0].id < 805){
+
+            generateImgAndVideo('img/noche-nublado.png', 'Luna cubierta por nubes de lluvia', 'img/nublado.mp4');
+
+
+        }else if( datosWeater[0].id === 511 ||datosWeater[0].id >= 600 && datosWeater[0].id <= 602 || datosWeater[0].id >= 611 && datosWeater[0].id <= 613 || datosWeater[0].id === 615 || datosWeater[0].id === 616 || datosWeater[0].id >= 620 && datosWeater[0].id <= 622){
+
+            generateImgAndVideo('img/nieve.png', 'Nube con copos de nieve', 'img/nieve-2.mp4');
+
+
+        }else if(datosWeater[0].id >= 300 && datosWeater[0].id <= 302 || datosWeater[0].id >= 310 && datosWeater[0].id <= 314 || datosWeater[0].id === 321 || datosWeater[0].id >= 500 && datosWeater[0].id <= 504 || datosWeater[0].id >= 520 && datosWeater[0].id <= 522 || datosWeater[0].id === 531){
+
+            generateImgAndVideo('img/lluvia.png', 'Nube de lluvia', 'img/nublado.mp4');
+
+
+        }else if(datosWeater[0].id >= 200 && datosWeater[0].id <= 202 || datosWeater[0].id >= 210 && datosWeater[0].id <= 212 || datosWeater[0].id === 221 || datosWeater[0].id >= 230 && datosWeater[0].id <= 232){
+
+            generateImgAndVideo('img/tormenta.png', 'Nube de tormenta', 'img/lluvia-2.mp4');
+
+
+        }else if(datosWeater[0].id === 781){
+
+            generateImgAndVideo('img/tornado.png', 'Tornado', 'img/lluvia-2.mp4');
+
+
+        }else{
+
+            generateImgAndVideo('noche-niebla.png', 'Luna cubierta por nubes oscuras', 'img/luna-niebla.mp4');
+
+        }
+    }
+}
+
+/**
+ * Setea la imagen y el video del clima según los parámetros proporcionados
+ *
+ * @param imgString
+ * @param altString
+ * @param videoString
+ */
+function generateImgAndVideo(imgString, altString, videoString){
+    img.src= imgString;
+    img.alt= altString;
+
+    let video = document.createElement('video');
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+    divVideo.appendChild(video);
+
+    let source = document.createElement('source');
+    source.src= videoString;
+    source.type="video/mp4";
+
+    video.appendChild(source);
+}
+
+/**
+ * Genera y setea el mapa.
+ * @param latitud
+ * @param longitud
+ */
 function initMap(latitud, longitud) {
     
     console.log(latitud, longitud);
